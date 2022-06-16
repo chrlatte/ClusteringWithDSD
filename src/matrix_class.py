9c2e4b694794af22b81c54d9d45329a8ecbadc48
Charlotte A. Versavel <cversa01@vm-hw02.eecs.tufts.edu>
2022-06-16 15:35:38 -0400

    fixed bug in initializer where i was only adding in interactions once into the table

--- src/matrix_class.py
+++ src/matrix_class.py
@@ -25,7 +25,7 @@ class ProteinMatrix:
 
 
 
-    def __init__(self, csv_filename):
+    def __init__(self, csv_filename: str):
 
         self.protein_data_df = pd.read_csv(csv_filename, delimiter = '\s+', names = ["gene_1", "gene_2", "edge_weight"]) 
 
@@ -50,14 +50,13 @@ class ProteinMatrix:
 
 
 
-    """             _init_dict_of_proteins_and_indexes(self)
-        Parameters: none
+
+    def _init_dict_of_proteins_and_indexes(self):
+        """             
         Purpose:    a helper function to populate a the protein's index 
                     dictionary 
         Returns:    n/a
-    """
-    def _init_dict_of_proteins_and_indexes(self):
-
+        """
         index = 0
         for protein in self.list_of_all_proteins_in_matrix:
             self.protein_indexes[protein] = index
@@ -65,20 +64,27 @@ class ProteinMatrix:
         
 
 
-    """             _init_matrix(self)
-        Parameters: none
-        Purpose:    a helper function to populate the matrix with interactions
-        Returns:    n/a
-    """
+
     def _init_matrix(self):
-        self.protein_matrix = pd.DataFrame(columns=self.list_of_all_proteins_in_matrix, index=self.list_of_all_proteins_in_matrix)
-        self.protein_matrix.fillna(0,inplace=True)
+        """             
+            Purpose:    a helper function to populate the matrix with interactions from a csv file
+            Returns:    n/a
+        """
+        self.protein_matrix = pd.DataFrame(
+            columns=self.list_of_all_proteins_in_matrix, 
+            index=self.list_of_all_proteins_in_matrix)
         
-        # take each pair of proteins, find their indexes, and then populate the matrix with the proteins
+        self.protein_matrix.fillna(0, inplace=True)
+        
+        # take each pair of proteins, find their indexes, and then populate the matrix with their interaction
         for n in range(len(self.protein_data_df)): 
             index1 = self.protein_indexes[self.protein_data_df.iloc[n, 0]]
             index2 = self.protein_indexes[self.protein_data_df.iloc[n, 1]]
-            self.protein_matrix.iloc[index1, index2] = self.protein_data_df.iloc[n, 2]
+            num = self.protein_data_df.iloc[n, 2]
+
+            self.protein_matrix.iloc[index1, index2] = num
+            self.protein_matrix.iloc[index2, index1] = num
+
         
 
             
