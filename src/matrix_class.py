6994dd2204f07a3097cb810131a81096ed1e116b
Charlotte A. Versavel <cversa01@vm-hw02.eecs.tufts.edu>
2022-06-16 15:19:54 -0400

    finished initialization for csv file

--- src/matrix_class.py
+++ src/matrix_class.py
@@ -17,25 +17,25 @@ class ProteinMatrix:
     member variables:
     """
 
-    protein_matrix_df = pd.DataFrame
-    proteins_in_matrix = np.array
+    protein_data_df = pd.DataFrame
+    list_of_all_proteins_in_matrix = np.array
     protein_indexes = dict()
+    protein_matrix = pd.DataFrame()
+
 
 
 
     def __init__(self, csv_filename):
-        # self.type = #dataframe?
-        # read in genes and interactions into a dataframe
-        
-        # print(f"filename: {csv_filename}")
 
-        self.protein_matrix_df = pd.read_csv(csv_filename, delimiter = '\s+', names = ["gene_1", "gene_2", "edge_weight"]) 
+        self.protein_data_df = pd.read_csv(csv_filename, delimiter = '\s+', names = ["gene_1", "gene_2", "edge_weight"]) 
 
-        self.proteins_in_matrix = np.unique(np.append(self.protein_matrix_df["gene_1"], self.protein_matrix_df["gene_2"]))
-
+        self.list_of_all_proteins_in_matrix = np.unique(np.append(self.protein_data_df["gene_1"], self.protein_data_df["gene_2"]))
         self._init_dict_of_proteins_and_indexes()
+
+        self._init_matrix()
+
+
         
-        #print(self.protein_matrix_df)
     
     
 
@@ -43,8 +43,8 @@ class ProteinMatrix:
         with pd.option_context('display.max_rows', 10,
                         'display.max_columns', 10,
                         'display.precision', 5):
-                print(self.protein_matrix_df)
-        
+                print(self.protein_matrix)
+                # print(self.protein_data_df)
         return ""
 
 
@@ -59,8 +59,27 @@ class ProteinMatrix:
     def _init_dict_of_proteins_and_indexes(self):
 
         index = 0
-        for protein in self.proteins_in_matrix:
+        for protein in self.list_of_all_proteins_in_matrix:
             self.protein_indexes[protein] = index
             index += 1
         
+
+
+    """             _init_matrix(self)
+        Parameters: none
+        Purpose:    a helper function to populate the matrix with interactions
+        Returns:    n/a
+    """
+    def _init_matrix(self):
+        self.protein_matrix = pd.DataFrame(columns=self.list_of_all_proteins_in_matrix, index=self.list_of_all_proteins_in_matrix)
+        self.protein_matrix.fillna(0,inplace=True)
         
+        # take each pair of proteins, find their indexes, and then populate the matrix with the proteins
+        for n in range(len(self.protein_data_df)): 
+            index1 = self.protein_indexes[self.protein_data_df.iloc[n, 0]]
+            index2 = self.protein_indexes[self.protein_data_df.iloc[n, 1]]
+            self.protein_matrix.iloc[index1, index2] = self.protein_data_df.iloc[n, 2]
+        
+
+            
+            
