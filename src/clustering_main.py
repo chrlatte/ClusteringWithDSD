0aa40b5aabd0b373f399b7c40227f9b3492aa753
Charlotte A. Versavel <cversa01@vm-hw02.eecs.tufts.edu>
2022-06-16 15:19:00 -0400

    minor style changes

--- src/clustering_main.py
+++ src/clustering_main.py
@@ -8,13 +8,16 @@ def get_args():
     python clustering_main.py --network "../data/networks/DREAM_files/dream_3.txt"
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--network", help = "Network file")
+    parser.add_argument("--network", "-n", help="Network file", required=True)
+
+
     return parser.parse_args()
 
 
 def main(arg):
+
     network = arg.network
-    df = pd.read_csv(network, sep = " ", header = None)
+    df = pd.read_csv(network, sep=" ", header=None)
     nodes = set(list(df[0]) + list(df[1]))
     n = len(nodes)
 
@@ -24,6 +27,8 @@ def main(arg):
     A = np.zeros((n, n))
     A[4,5]
     print(df)
+    print(f"done")
+
 
 if __name__ == "__main__":
     main(get_args())
