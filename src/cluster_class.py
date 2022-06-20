06ebb5e87a490110357434fb4fa7af77301031ff
Charlotte A. Versavel <cversa01@vm-hw01.eecs.tufts.edu>
2022-06-20 12:58:00 -0400

    started work on the cluster class

--- src/cluster_class.py
+++ src/cluster_class.py
@@ -12,3 +12,32 @@ import pandas as pd
 import numpy as np
 
 class ProteinCluster:
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    * * * * * * * * * * * * * MEMBER VARIABLES * * * * * * * * * * * * * *  
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    df_row = pd.Series() # display(df.iloc[n])
+
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    * * * * * * * * * * * * * * INITIALIZERS * * * * * * * * * * * * * * *  
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    def __init__(self, row = None):
+        """             
+        Purpose:    to populate the submatrix with data from the original 
+                    matrix. 
+        Returns:    n/a
+        """
+        self.df_row = row
+        pass
+
+
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    * * * * * * * * * * * * * * * GETTERS * * * * * * * * * * * * * * * * *  
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+    def get_index(self,  protein : str or None = None) -> dict() or int:
+        """             
+        Purpose:    to allow access to the protein->index dictionary
+        Returns:    either the index for a specific protein or a dictionary of 
+                    protein names and their indexes
+        """
+        pass
--- src/cluster_main.py
+++ src/cluster_main.py
@@ -14,9 +14,12 @@ from cluster_class import *
 def main():
 
     # read in the cluster file into a dataframe
-    cluster_df = pd.read_csv("../data/testing_data/cluster_test.txt", sep="\t", header=0)
+    cluster_df = pd.read_csv("../data/testing_data/small_cluster.txt", sep="\t", header=0) # TODO: pandas.errors.ParserError: Error tokenizing data. C error: Expected 39 fields in line 31, saw 92 -> need to read line by line instead of reading in the entire file and have it create arrays of the data
+    
+    print(cluster_df)
+
     # create a cluster object
-    cluster = Cluster(cluster_df)
+    cluster = ProteinCluster(cluster_df)
 
     # print the cluster object
     print(cluster)
