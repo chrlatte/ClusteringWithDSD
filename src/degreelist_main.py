2eb677174bf91b6cc4538decb90997ed73cb98d8
Charlotte A. Versavel <cversa01@vm-hw01.eecs.tufts.edu>
2022-06-20 16:49:36 -0400

    created a degreelist main that caused an error. will need to make edits to clustering class

---
+++ src/degreelist_main.py
@@ -0,0 +1,30 @@
+"""
+Author: Charlotte Versavel
+Date:   June 2022
+
+                             degreelist_main.py
+
+Purpose: a main to test the matrix and cluster classes together, focusing mainly on the degreelist generation and implementation
+
+"""
+
+from matrix_class import *
+from cluster_class import *
+
+def main():
+
+    matrix = ProteinMatrix("../data/testing_data/tiny_dream3.txt")
+
+    matrix._init_degree_list_()
+
+    clusters = ProteinClusters("../data/testing_data/fake_cluster.txt")
+
+    print(clusters)
+    clusters.print_all()
+
+     
+    
+
+
+if __name__ == "__main__":
+    main()
