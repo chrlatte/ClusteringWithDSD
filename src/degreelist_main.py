2abf85e238b3700c69d0750089a9d37462c8a1b9
Charlotte A. Versavel <cversa01@vm-hw03.eecs.tufts.edu>
2022-06-22 11:02:17 -0400

    seems to work for finding proteins linked with large datasets

--- src/degreelist_main.py
+++ src/degreelist_main.py
@@ -16,7 +16,7 @@ def main():
 
     matrix = ProteinMatrix("../data/networks/DREAM_files/dream_3.txt")
     # print(f"Matrix:\n{matrix}")
-    
+
     clusters = ProteinClusters("../data/clusters/3344522.7320912.1_ppi_anonym_v2.txt")
     # print(f"Clusters:\n{clusters}")
     # clusters.print_all()
@@ -28,23 +28,23 @@ def main():
 
     #########
     
-    individual_cluster = clusters.get_cluster(0)
-    clusters_matrix = SubMatrix(individual_cluster.get_protein_list(), matrix)
-    print()
+    # individual_cluster = clusters.get_cluster(0)
+    # clusters_matrix = SubMatrix(individual_cluster.get_protein_list(), matrix)
+    # print()
 
 
-    print(f"using cluster: {individual_cluster}")
-    print(f"using list of proteins: {matrix.get_list_of_proteins()}")
-    print(f"visual representation of cluster:\n{clusters_matrix.get_matrix()}")
-
-
-    result = degreelist.create_list_of_proteins_connected_to_cluster(matrix.get_list_of_proteins(), individual_cluster.get_protein_list())
-
+    
+    for n in range(clusters.get_num_clusters()):
+        result = degreelist.create_list_of_proteins_connected_to_cluster(matrix.get_list_of_proteins(), clusters.get_proteins_from_cluster(n))
+        print(f"THE PROTEINS WITH >3 CONNECTIONS TO CLUSTER: {n} ARE {result}")
 
 
 
+    # print(f"using cluster: {individual_cluster}")
+    # print(f"using list of proteins: {matrix.get_list_of_proteins()}")
+    # print(f"visual representation of cluster:\n{clusters_matrix.get_matrix()}")
 
-    print(f"THE PROTEINS WITH >3 CONNECTIONS TO CLUSTER: {individual_cluster} ARE {result}")
+    # print(f"THE PROTEINS WITH >3 CONNECTIONS TO CLUSTER: {individual_cluster} ARE {result}")
     
 
     
