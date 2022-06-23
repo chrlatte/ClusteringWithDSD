76efaabd065e6d8d12b83b243e9df6798ab11c54
Charlotte A. Versavel <cversa01@vm-hw04.eecs.tufts.edu>
2022-06-23 14:34:15 -0400

    turns out submatrixes were not being properly initialized

--- src/connected_components_main.py
+++ src/connected_components_main.py
@@ -13,8 +13,8 @@ from cluster_class import *
 from degreelist_class import *
 
 # from scipy.sparse import coo_matrix
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import connected_components
+# from scipy.sparse import csr_matrix
+# from scipy.sparse.csgraph import connected_components
 
 
 def main():
@@ -27,10 +27,10 @@ def main():
     actual_cluster_file = "../data/clusters/3344522.7320912.1_ppi_anonym_v2.txt"
 
 
-    matrix = ProteinMatrix(testing_matrix_file)
+    matrix = ProteinMatrix(actual_matrix_file)
     # print(f"Matrix:\n{matrix}")
 
-    clusters = ProteinClusters(testing_cluster_file)
+    clusters = ProteinClusters(actual_cluster_file)
     # print(f"Clusters:\n{clusters}")
     # clusters.print_all()
 
@@ -40,11 +40,12 @@ def main():
 
     # want to take a cluster and then make a submatrix. 
     # the submatrix houses the CSR matrix
-    for i in range(1): # clusters.get_num_clusters()
+    for i in range(clusters.get_num_clusters()): # clusters.get_num_clusters()
 
         submatrix = SubMatrix(clusters.get_cluster_proteins(i), matrix)
-        n, labels = submatrix.get_num_components_and_labels()
-        print(f"Cluster {i} has {n} components with labels {labels}")
+        print(submatrix.get_matrix())
+        #  n, labels = submatrix.get_num_components_and_labels()
+        # print(f"Cluster {i} has {n} components with labels {labels}")
 
 
     # matrix_csr = csr_matrix(matrix.get_matrix().astype(pd.SparseDtype(dtype=float, fill_value=0)))
