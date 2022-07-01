aa19164ebee2a9759802fa8e20b494ba610f4c40
Charlotte A. Versavel <cversa01@vm-hw07.eecs.tufts.edu>
2022-07-01 09:36:12 -0400

    ran program on dream3

--- src/connected_components_main.py
+++ src/connected_components_main.py
@@ -9,6 +9,8 @@ Purpose: a main to use the scipy library to determine connected components in a 
 """
 
 import numpy as np
+import json
+
 
 from matrix_class import ProteinMatrix
 from matrix_class import SubMatrix
@@ -19,29 +21,30 @@ from degreelist_class import DegreeList
 from connected_components_utils import *
 
 
-
 def main():
 
     testing_matrix_file = "../data/testing_data/fake_cluster_dream.txt"
     testing_cluster_file = "../data/testing_data/fake_cluster.txt"
 
     dream3_matrix_file = "../data/networks/DREAM_files/dream_3.txt"
-    dream3_cluster_file = "../data/results/DREAM-3-cc/d3_5_100.json-cluster.json"
+    
+    dream3_cluster_dict = "../data/results/DREAM-3-cc/d3_5_100.json-cluster.json"
+    dict_of_clusters = {}
+    # # convert actual cluster file to a dictionary!!
+    with open(dream3_cluster_dict,"r") as cluster_dict_file:
+        dict_of_clusters = json.load(cluster_dict_file)
+    
 
     dream2_matrix_file = "../data/networks/DREAM_files/dream_2.txt"
     dream3_old_cluster_file = "../data/clusters/3344522.7320912.1_ppi_anonym_v2.txt"
 
 
-    matrix = ProteinMatrix(testing_matrix_file)
+    matrix = ProteinMatrix(dream3_matrix_file)
     # print(f"Matrix:\n{matrix}")
 
 
-    # dict_of_clusters = {}
-    # # convert actual cluster file to a dictionary!!
-    # with open(,"r") as cluster_dict_file:
-    #     dict_of_clusters = json.load(cluster_dict_file)
     
-    clusters = AllClusters(testing_cluster_file)
+    clusters = AllClusters(protein_to_cluster_dict=dict_of_clusters)
     # # print(f"Clusters:\n{clusters}")
     # clusters.print_all()
 
