e59ed1428ba957dfa2234c1dcdf7a8d0da01c248
Charlotte A. Versavel <cversa01@vm-hw06.eecs.tufts.edu>
2022-07-05 18:21:19 -0400

    work in progress

--- src/reworked_clusters_main.py
+++ src/reworked_clusters_main.py
@@ -38,12 +38,16 @@ def main():
     dream3_old_cluster_file = "../data/clusters/3344522.7320912.1_ppi_anonym_v2.txt"
 
 
-    matrix = ProteinMatrix(dream3_matrix_file)
+    # matrix = ProteinMatrix(dream3_matrix_file)
+    matrix = ProteinMatrix(testing_matrix_file)
     # print(f"Matrix:\n{matrix}")
 
 
     
-    clusters = AllClusters(protein_to_cluster_dict=dict_of_clusters)
+    # clusters = AllClusters(protein_to_cluster_dict=dict_of_clusters)
+    clusters = AllClusters(testing_cluster_file)
+
+
     # # print(f"Clusters:\n{clusters}")
     # clusters.print_all()
 
@@ -54,8 +58,20 @@ def main():
 
     #foo.print_all_clusters_and_connected_proteins()
 
-    foo.find_clusters_that_match_criteria(ratio = 1, b = 0)
- 
+    # print(degreelist.determine_num_edges_to_cluster(protein, clusters.get_cluster_proteins(cluster_num), max_edges_until_return=3, also_return_which_proteins=True))
+    
+    
+
+
+
+    qualifying_clusters = foo.find_clusters_that_match_criteria(ratio = 1, b = 0)
+    # print(qualifying_clusters)
+
+    # for cluster in qualifying_clusters:
+        
+    print(foo.find_proteins_that_match_criteria(1, ratio = 0, b = 0, max_degree=600))
+        
+    
     
 if __name__ == "__main__":
     main()
