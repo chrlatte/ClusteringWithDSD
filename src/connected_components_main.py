9245505358a2f3d8af55fd77b59590641b308464
charlotte <charlotte.versavel@tufts.edu>
2022-07-21 12:28:32 -0400

    added a function to determine an apporpriate ratio

--- src/connected_components_main.py
+++ src/connected_components_main.py
@@ -27,7 +27,7 @@ def main():
     testing_cluster_file = "../data/testing_data/fake_cluster.txt"
 
     dream3_matrix_file = "../data/networks/DREAM_files/dream_3.txt"
-    dream3_cluster_file = "../data/results/DREAM-3-cc/d3_5_100.json-cluster.json"
+    dream3_cluster_file = "../data/results/DREAM-3-cc/d3_5_100.json-cluster.json" 
 
     dream3_clusters_dict = {}
     # convert actual cluster file to a dictionary!!
@@ -42,10 +42,8 @@ def main():
 
     # Dream3 Data:
     clusters = AllClusters(protein_to_cluster_dict=dream3_clusters_dict)
-
-    # matrix = ProteinMatrix(dream3_matrix_file)
-    # degreelist = DegreeList(matrix)
-
+    matrix = ProteinMatrix(dream3_matrix_file)
+    degreelist = DegreeList(matrix)
 
     # print(f"Matrix:\n{matrix}")
     # print(f"Clusters:\n{clusters}")
@@ -53,11 +51,12 @@ def main():
     # print(f"Degree list:\n{degreelist}")
 
 
-    # first, going to find the clusters that are moderatly connected (by dream3 standards) (for dict0, there are 0 clusters with a .75, 1 that fits .8 & .85, 4 with .9) (for dict1 there are lots of clusters for ratio .9, lots for ratio = .7) CHOOSING TO USE DICT1 so RATIO = .5 -> clusters must be more than 50% connected
-    clusters_that_are_somewhat_connected = find_clusters_that_match_criteria(matrix, clusters, degreelist)
-
+    # first, going to find the clusters that are moderatly connected (by dream3 standards)
+    clusters_that_are_somewhat_connected = find_clusters_that_match_criteria(matrix, clusters, degreelist, ratio=pick_ratio(clusters.get_num_clusters()))
 
     print(f"clusters_that_are_somewhat_connected: {clusters_that_are_somewhat_connected}")
+
+    
 
 if __name__ == "__main__":
     main()
--- src/connected_components_utils.py
+++ src/connected_components_utils.py
@@ -18,50 +18,6 @@ import numpy as np
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
  * * * * * * * * * * * * * * * FUNCTIONS * * * * * * * * * * * * * * *
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# def print_single_cluster_and_connected_proteins(x: int, matrix: ProteinMatrix, clusters: AllClusters, degreelist: DegreeList):
-#     """
-#     TODO
-#     """
-#     submatrix = SubMatrix(clusters.get_cluster_proteins(x), matrix)
-#     num_components, labels = submatrix.get_num_components_and_labels()
-    
-#     print(f"Cluster {x} has {num_components} components: {[list(np.array(submatrix.get_list_of_proteins())[np.nonzero(labels == i)]) for i in range(num_components)]}.")
-
-#     if num_components == 1: 
-#         print(f"Cluster {x} is fully connected. will not search for attached proteins")
-    
-#     else:
-
-#         component_dictionary = dict() # protein : component_num
-#         j = 0
-#         for array in [(np.array(submatrix.get_list_of_proteins())[np.nonzero(labels == i)]) for i in range(num_components)]:
-#             for protein in array:
-#                 component_dictionary[protein] = j
-#             j += 1
-
-#         list_of_proteins_connected_to_cluster = degreelist.create_list_of_proteins_connected_to_cluster(degreelist.get_list_of_proteins_sorted_by_degree(), clusters.get_cluster_proteins(x), min_num_connections=3)
-
-
-#         for protein in list_of_proteins_connected_to_cluster:
-#             # print(f"{protein} is has 3+ connections to {matrix.get_list_of_proteins_connected_to_protein(protein)}")
-#             which_components = degreelist.which_components_of_a_cluster_would_a_protein_connect(protein, clusters.get_cluster_proteins(x), component_dictionary)
-
-#             if len(which_components) == num_components:
-#                 print(f"protein {protein} has degree {matrix.find_degree(protein)} and will connect ALL {num_components} components!!!!!!!!!")
-            
-#             elif len(which_components) > 1:
-#                 print(f"protein {protein} has degree {matrix.find_degree(protein)} and will connect {len(which_components)} components: {which_components}")
-
-
-# def print_all_clusters_and_connected_proteins(matrix: ProteinMatrix, clusters: AllClusters, degreelist: DegreeList):
-#     """
-#     TODO
-#     """
-#     for x in range(clusters.get_num_clusters()):
-#         print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
-#         print_single_cluster_and_connected_proteins(x, matrix, clusters, degreelist)
-
-
 def find_clusters_that_match_criteria(matrix: ProteinMatrix, clusters: AllClusters, degreelist: DegreeList, ratio: float = .5, constant: int = 0) -> list():
     """
     Parameters: 
@@ -89,44 +45,20 @@ def find_clusters_that_match_criteria(matrix: ProteinMatrix, clusters: AllCluste
     return cluster_nums_that_qualify
 
 
-
-# def create_dict_of_proteins_to_add_to_clusters(x: int, matrix: ProteinMatrix, clusters: AllClusters, degreelist: DegreeList) -> dict():
-#     """
-#     returns a dict in a form  TODO
-#     """
-#     proteins_to_add_back = dict()
-
-#     submatrix = SubMatrix(clusters.get_cluster_proteins(x), matrix)
-#     num_components, labels = submatrix.get_num_components_and_labels()
+def pick_ratio(num_clusters: int):
+    """
+    will determine an approximate ratio to start with based on the total number of clusters
+    """
+    if (num_clusters > 1000):
+        return .5
+    elif num_clusters > 500:
+        return .7
+    elif num_clusters > 200:
+        return .9
+    elif num_clusters > 100:
+        return .95
+    elif num_clusters > 50:
+        return .995
+    else: 
+        return 1
     
-#     print(f"Cluster {x} has {num_components} components: {[list(np.array(submatrix.get_list_of_proteins())[np.nonzero(labels == i)]) for i in range(num_components)]}.")
-
-#     if num_components == 1: 
-#         print(f"Cluster {x} is fully connected. will not search for attached proteins")
-    
-#     else:
-
-#         component_dictionary = dict() # protein : component_num
-#         j = 0
-#         for array in [(np.array(submatrix.get_list_of_proteins())[np.nonzero(labels == i)]) for i in range(num_components)]:
-#             for protein in array:
-#                 component_dictionary[protein] = j
-#             j += 1
-
-#         list_of_proteins_connected_to_cluster = degreelist.create_list_of_proteins_connected_to_cluster(degreelist.get_list_of_proteins_sorted_by_degree(), clusters.get_cluster_proteins(x), min_num_connections=3)
-
-
-#         for protein in list_of_proteins_connected_to_cluster:
-#             # print(f"{protein} is has 3+ connections to {matrix.get_list_of_proteins_connected_to_protein(protein)}")
-#             which_components = degreelist.which_components_of_a_cluster_would_a_protein_connect(protein, clusters.get_cluster_proteins(x), component_dictionary)
-
-#             if len(which_components) == num_components:
-#                 print(f"protein {protein} has degree {matrix.find_degree(protein)} and will connect ALL {num_components} components!!!!!!!!!")
-            
-#             elif len(which_components) > 1:
-#                 print(f"protein {protein} has degree {matrix.find_degree(protein)} and will connect {len(which_components)} components: {which_components}")
-
-
-
-#     return proteins_to_add_back
-
